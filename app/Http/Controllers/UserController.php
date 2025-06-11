17fb15089fc97b9a18c370ca534787351c0ce592
Charles <charlesferraren9@gmail.com>
2025-06-11 21:58:47 +0800

    Activity 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users',
-            'password' => 'required|min:6',
+            'password' => 'required|string|min:8|confirmed',
         ]);
 
         // Create the user
